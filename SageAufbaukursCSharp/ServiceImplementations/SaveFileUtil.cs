44050b0a5e6df05863de4b6a859e92048bf03235
SchulungsTeilnehmer <Torsten.Lubomirski@sage.com>
2019-02-06 16:31:31 +0100

    TEST3

--- SageAufbaukursCSharp/ServiceImplementations/SaveFileUtil.cs
+++ SageAufbaukursCSharp/ServiceImplementations/SaveFileUtil.cs
@@ -29,12 +29,6 @@ namespace SageAufbaukursCSharp.ServiceImplementations
         #endregion
         public bool Save(object beleg, string path)
         {
-            throw new NotImplementedException();
-        }
-
-        public bool Save(object beleg)
-        {
-
             try
             {
 
@@ -42,7 +36,76 @@ namespace SageAufbaukursCSharp.ServiceImplementations
                 {
                     swDatei.WriteLine(beleg);
                 }
+                return true;
+            }
+            catch (PathTooLongException ptl)
+            {
+                string DateiName = Path.GetFileNameWithoutExtension(path);
+                string DateiExtension = Path.GetExtension(path);
+                string DateiPfad = Path.GetFullPath(path);
+                string sNeuerPfad = string.Empty;
+                int Frei = 260 - DateiPfad.Length - DateiExtension.Length - 1;
+                if (Frei > 1)
+                {
+                    sNeuerPfad = String.Concat(DateiPfad, DateiName, ".", DateiExtension);
+                    SaveFileUtil Ergebnis = new SaveFileUtil();
+                    bool gings = Ergebnis.Save(beleg, sNeuerPfad);
+                    if (string.IsNullOrEmpty(Ergebnis.Message))
+                    {
+                        Message = ptl;
+                    }
+                    return Ergebnis
+
+                }
+                else
+                {
+                    using (var swDatei1 = new StreamWriter(sDefaultPfad + "TEXT.TXT"))
+                        Message = ae.Message;
+                    Fault = ae;
+                    return false;
+                }
+                  
+
+
+
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                throw;
+            }
+            catch (ArgumentException ae)
+            {
+                using (var swDatei1 = new StreamWriter(sDefaultPfad + "TEXT.TXT"))
+                    Message = ae.Message;
+                Fault = ae;
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Fault = ex;
+
+                return false;
+            }
+            return true;
+        }
+    
+
+        public bool Save(object beleg)
+        {
+
+            try
+            {
+            
+                using (var swDatei = new StreamWriter(""))
+                {
+                    swDatei.WriteLine(beleg);
+                }
                 return true;    
+            }
+            catch (PathTooLongException ptl)
+            {
+                string DateiName = Path.GetFileName(path);
+
             }
             catch (UnauthorizedAccessException uae)
             {
