b37fd3e88a28f71c9967e3742961d5205fd2c13b
SchulungsTeilnehmer <Torsten.Lubomirski@sage.com>
2019-02-06 11:20:11 +0100

    1.TEST

---
+++ SageAufbaukursCSharp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using System.ComponentModel;
+namespace SageAufbaukursCSharp.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion INotifyPropertyChanged
+
+
+        //TEST
+        public void NotifyPropertyChanged([CallerMemberName]string nameProp = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameProp));
+        }
+    }
+}
